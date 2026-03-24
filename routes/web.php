adc53a1fc8c85c25565c8c6d3d85249ba71a4251
pabo1 <handanil863@gmail.com>
2026-03-24 15:21:40 +0500

    style this

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
 
 Route::get('/', [ChirpController::class, 'index']);
 
@@ -21,11 +22,12 @@ Route::view('/register', 'auth.register')
 Route::post('/register', Register::class)
     ->middleware('guest');
 
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+Route::post('/login', Login::class)
+    ->middleware('guest');
+// Logout route
 Route::post('/logout', Logout::class)
     ->middleware('auth')
     ->name('logout');
-Route::view('/login', 'auth.login')
-    ->middleware('guest')->middleware(guest)
-    ->name('login');
-Route::post('login', Login::class)
-    ->name('login');
